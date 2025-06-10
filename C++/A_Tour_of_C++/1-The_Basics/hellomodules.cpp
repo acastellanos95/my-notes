bc2a7b975eca06be4e8eb2a5f9f27a6c547107c1
Andre Castellanos <acastellanosaldama@gmail.com>
2025-06-09 20:10:06 -0600

    moved files

--- C++/A_Tour_of_C++/1-The_Basics/hellomodules.cpp
+++ C++/A_Tour_of_C++/1-The_Basics/hellomodules.cpp
@@ -1,11 +1,13 @@
 import std;
 
+using namespace std;
+
 struct example {
     int hi;
 };
 
 int main (int argc, char *argv[]) {
-    example ex{1}
-    std::cout << "hello module!\n";
+    example ex{1};
+    cout << "hello module!\n";
     return 0;
 }
